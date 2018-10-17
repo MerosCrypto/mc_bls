6d846d553a8ad7866c1c786b785860d19378b970
Luke Parker <lukep@digispend.io>
2018-10-16 21:15:57 -0500

    PrivateKey creation and serialization.

---
+++ BLS.nimble
@@ -0,0 +1,6 @@
+version     = "0.0.1"
+author      = "Luke Parker"
+description = "A Nim Wrapper for Chia's BLS Library."
+license     = "MIT"
+
+requires "nim > 0.18.0"
---
+++ tests/privateKey.nim
@@ -0,0 +1,7 @@
+import ../BLS
+
+var
+    key: string = "003206F418C701193458C013120C5906DC12663AD1520C3E596EB6092C14FE16"
+    privKey: BLSPrivateKey = newBLSPrivateKey(key)
+
+assert($privKey == key)
